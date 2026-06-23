133e48fe2b50c7b3c0a8a7c5731e94726cf0d8ce
PREDATOR <evgeniy.li2016@gmail.com>
2026-06-23 12:40:26 +0300

    [main] - the task is completed

--- src/test/java/com/testframework/utils/test_constants/TestConstants.java
+++ src/test/java/com/testframework/utils/test_constants/TestConstants.java
@@ -1,14 +1,5 @@
 package com.testframework.utils.test_constants;
 
 public interface TestConstants {
-    String DARK = "Dark";
-    String LIGHT = "Light";
-    String ENGLISH = "en";
-    String PERSIAN = "fa";
-    String MEDIUM = "medium";
-    String EASY = "easy";
-    String HARD = "hard";
-    String YOU_WIN = "You win!";
-    String EASY1 = "Easy";
-    String WIN = "Win";
+
 }
